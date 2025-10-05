0764232ba6c56e879f63634595d9b05bf11ae60c
Grant Smith <14.gsmith.14@gmail.com>
2025-10-05 18:17:06 -0500

    added nix stuff

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "vs code extension dev env";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            vsce
+          ];
+        };
+      }
+    );
+}
